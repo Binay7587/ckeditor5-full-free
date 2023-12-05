badea0509bc3e5f725ca863c54fb0dde3d3a9f79
Binaya Karki <riddhasoft@192.168.254.4>
2023-12-05 20:43:28 +0545

    docs: updated pages

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ CKEditor&nbsp;5 Classic Editor Full Free Build
 
 The classic editor build for CKEditor&nbsp;5. Read more about the [classic editor build](https://ckeditor.com/docs/ckeditor5/latest/installation/getting-started/predefined-builds.html#classic-editor) and see the [demo](https://ckeditor.com/docs/ckeditor5/latest/examples/builds/classic-editor.html).
 
-![CKEditor&nbsp;5 classic editor build screenshot](https://c.cksource.com/a/1/img/npm/ckeditor5-build-classic.png)
+![CKEditor&nbsp;5 Classic Editor Full Free Build Screenshot](./docs/ckeditor-full-free-demo.png)
 
 ## Documentation
 
--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,21 @@
-createDialog().then( config => {
-	return ClassicEditor
-		.create( document.querySelector( '.editor' ), {
-			licenseKey: config.licenseKey
-		} )
-		.then( editor => {
-			window.editor = editor;
-		} )
-		.catch( handleSampleError );
-} );
+// createDialog().then(config => {
+//     return ClassicEditor
+//         .create(document.querySelector('.editor'), {
+//             licenseKey: config.licenseKey
+//         })
+//         .then(editor => {
+//             window.editor = editor;
+//         })
+//         .catch(error => {
+//             console.error('There was a problem initializing the editor.', error);
+//         });
+// });
 
-function handleSampleError( error ) {
-	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
-
-	const message = [
-		'Oops, something went wrong!',
-		`Please, report the following error on ${ issueUrl } with the build id "slhanul9n97u-l5wyz7rg4rb1" and the error stack trace:`
-	].join( '\n' );
-
-	console.error( message );
-	console.error( error );
-}
+ClassicEditor
+    .create(document.querySelector('.editor'))
+    .then(editor => {
+        window.editor = editor;
+    })
+    .catch(error => {
+        console.error('There was a problem initializing the editor.', error);
+    });
